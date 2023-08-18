68d9fd9711117d0742995cb94407d60e7ccb9c66
mdrafi276 <mdrafi37473@gmail.com>
2023-08-18 00:02:22 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function calculateTriangleArea(){
+   const baseField = document.getElementById('triangle-base');
+   const baseValueText = baseField.Value;
+   const base = parseFloat(baseValueText);    
+    
+   const heigthField = document.getElementById('triangle-height');
+   const heigthValueText = heigthField.Value;
+   const height = parseFloat(heigthValueText);
+   console.log(height)
+}
